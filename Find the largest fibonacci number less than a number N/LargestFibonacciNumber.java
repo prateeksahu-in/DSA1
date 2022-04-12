886e5987d4bad48b7bfd09f4ff4bf39c07a19532
prateeksahu <prateeksahu0101@gmail.com>
2022-04-12 23:59:34 +0530

    DSA1 problem solved

---
+++ Find the largest fibonacci number less than a number N/LargestFibonacciNumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+
+class LargestFibonacciNumber{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-->0) {
+            int n = sc.nextInt();
+            int result = largestFibonacciNumber(n);
+            System.out.println(result);
+        }
+        sc.close();
+    }
+
+    static int largestFibonacciNumber(int n){
+        return 0;
+    }
+}
