9355e6d096af55bf868e37c4b85e3a5f6c9bc148
prateeksahu <prateeksahu0101@gmail.com>
2022-04-14 23:59:17 +0530

    solved

---
+++ Average height.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+class AverageHeight{
+    // NOTE: Please do not modify this function
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float a[] = new float[n];
+
+        for(int i = 0; i < n; i++)
+        {
+            a[i] = Float.parseFloat(sc.next());
+        }
+
+        float avgHeight = averageHeight(n, a);
+        System.out.println(avgHeight);
+    }
+
+    // TODO: Implement this method
+    static float averageHeight(int n, float[] a){
+    }
+}
