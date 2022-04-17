89d3f2fd66b7be44e19b42368b0dd9120e060c7e
prateeksahu <prateeksahu0101@gmail.com>
2022-04-17 23:44:03 +0530

    DSA1 problem solved

---
+++ Simple Math - Problems/Sum of given Series -2/SeriesSumII.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class SeriesSumII {
+    // NOTE: Please do not modify this function
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        double A = sc.nextDouble();
+        double R = sc.nextDouble();
+
+        double result = seriesSumII(A, R);
+
+        System.out.println(result);
+    }
+
+    // TODO: Implement this method
+    static double seriesSumII(double A, double R) {
+        return (A/(1-R));
+    }
+}
