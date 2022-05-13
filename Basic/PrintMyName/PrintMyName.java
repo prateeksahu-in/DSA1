2e7fdc699f6d3d197d07b15473e76b6778d60319
prateeksahu0101 <prateeksahu0101@gmail.com>
2022-05-14 01:10:20 +0530

    added

---
+++ Basic/PrintMyName/PrintMyName.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class PrintMyName{
+	public static void printMyName(String name){
+	
+		System.out.print(name);	
+
+	}
+
+	public static void main(String args[]){
+
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();		
+		printMyName(name);
+	}
+
+}
