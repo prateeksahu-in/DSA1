b3fc3ac756100835dc3c011c2bcd139607ed148d
prateeksahu <prateeksahu0101@gmail.com>
2022-05-15 19:11:51 +0530

    Strings

---
+++ Basic/String/Strings.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Strings {
+    public static void main(String[] args) {
+        
+        String name = "Prateek";
+        System.out.println(name);
+        
+        Scanner sc = new Scanner(System.in);
+        
+        name = sc.nextLine();
+        System.out.print(name);
+    }
+}
